fe99e22cf2dc2bf6aa4e77caee1263e9785a18ea
EdenGenetTasama <edentasama19@gmail.com>
2022-01-12 11:29:55 +0200

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        Student[] studentArray = new Student[]
+        {
+           new Student(0, "Eden" , "Tasama" , 7 , 25),
+           new Student(1, "Yafit" , "Samuel" , 8 , 28),
+           new Student(2, "Keren" , "Hailu" ,9 , 50),
+           new Student(3, "Tikva" , "Yosef" , 10 , 70),
+
+        };
+
+        // GET: Student
+        public ActionResult GetStudentName()
+        {
+            string studentName = studentArray[0].FirstName;
+            ViewBag.StudentName = studentName;
+            return View();
+        }
+
+        public ActionResult GetStudentInfo()
+        {
+            ViewBag.AllInfo = studentArray;
+            return View();
+        }
+
+        public ActionResult GetAllStudntsInfo()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Class { get; set; }
+        public int Age { get; set; }
+        public Student(int id, string firstName, string lastName, int @class, int age)
+        {
+            this.Id=id;
+            this.FirstName=firstName;
+            this.LastName=lastName;
+            this.Class=@class;
+            this.Age=age;
+        }
+
+
+
+    }
+}
