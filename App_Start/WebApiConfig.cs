30d1e300cf2f5b2ddc1f5abfd9948d86611a2bae
EdenGenetTasama <edentasama19@gmail.com>
2022-01-12 14:32:54 +0200

    add api controllers

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace WebApplication1
 {
@@ -10,6 +11,8 @@ namespace WebApplication1
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
---
+++ Controllers/api/StudentApiController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers.api
+{
+    public class StudentApiController : ApiController
+    {
+        List<Student> listOfStudents = new List<Student>()
+        {
+           new Student(0, "Eden" , "Tasama" , 7 , 25),
+           new Student(1, "Yafit" , "Samuel" , 8 , 28),
+           new Student(2, "Keren" , "Hailu" ,9 , 50),
+           new Student(3, "Tikva" , "Yosef" , 10 , 70),
+        };
+        // GET: api/StudentApi
+        public IHttpActionResult Get()
+        {
+            return Ok(listOfStudents);
+        }
+
+        // GET: api/StudentApi/5
+        public IHttpActionResult Get(int id)
+        {
+            Student student = listOfStudents.Find(item => item.Id == id);
+            return Ok(student);
+        }
+
+        // POST: api/StudentApi
+        public IHttpActionResult Post([FromBody] Student student)
+        {
+            Student studentAdd = new Student( 5,"Daniel" , "Tal" , 10 , 23 );
+            listOfStudents.Add(studentAdd);
+            return Ok("Added");
+        }
+
+        // PUT: api/StudentApi/5
+        public IHttpActionResult Put(int id, [FromBody] string value)
+        {
+            return Ok();
+        }
+
+        // DELETE: api/StudentApi/5
+        public IHttpActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
