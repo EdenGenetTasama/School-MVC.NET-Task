3a0c7e42ce4d0598395cf14246512d36dd3a5cd6
EdenGenetTasama <edentasama19@gmail.com>
2022-01-14 14:03:41 +0200

    add delete mathod , add expeptions

--- Controllers/api/StudentApiController.cs
+++ Controllers/api/StudentApiController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -15,38 +16,99 @@ namespace WebApplication1.Controllers.api
 
         public IHttpActionResult Get()
         {
-            return Ok(new { Student.listOfStudents });
+            try
+            {
+
+                return Ok(new { Student.listOfStudents });
+            }
+            catch (SqlException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // GET: api/StudentApi/5
         public IHttpActionResult Get(int id)
         {
-            Student student = Student.listOfStudents.Find(item => item.Id == id);
-            return Ok(new { student});
+            try
+            {
+                Student student = Student.listOfStudents.Find(item => item.Id == id);
+                return Ok(new { student });
+            }
+            catch (SqlException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // POST: api/StudentApi
         public IHttpActionResult Post([FromBody] Student student)
         {
-            Student.listOfStudents.Add(student);
-            return Ok("Added");
+            try
+            {
+                Student.listOfStudents.Add(student);
+                return Ok("Added");
+            }
+            catch (SqlException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // PUT: api/StudentApi/5
         public IHttpActionResult Put(int id, [FromBody] Student student)
         {
-            Student studentToUpdate = Student.listOfStudents.First(item => item.Id == id);
-            if (studentToUpdate !=  null)
+            try
             {
-                studentToUpdate.
+
+                Student studentToUpdate = Student.listOfStudents.First(item => item.Id == id);
+                if (studentToUpdate !=  null)
+                {
+                    studentToUpdate.FirstName = student.FirstName;
+                    studentToUpdate.LastName = student.LastName;
+                    studentToUpdate.Age = student.Age;
+                    studentToUpdate.Class = student.Class;
+                }
+                return Ok(studentToUpdate);
             }
-            return Ok();
+            catch (SqlException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // DELETE: api/StudentApi/5
         public IHttpActionResult Delete(int id)
         {
-            return Ok();
+            try
+            {
+                Student.listOfStudents.Remove(Student.listOfStudents.First(item => item.Id == id));
+                return Ok("Delete");
+            }
+            catch (SqlException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
